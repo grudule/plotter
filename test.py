d1161e1c3b9396ce49e3202cc1a59543014d3ed3
Alexandre <alsag3@ulaval.ca>
2023-01-17 09:34:02 -0500

    adding dissociate font size change

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 from plotter.plotter import Plotter
 import numpy as np
 x_data = np.linspace(0, np.pi, 100)
-#x_data = (0,np.pi, 1000)
-y_data = {"valeur intensité": np.cos(x_data)**2}
+y_data = {"valeur intensité": (np.cos(x_data)**2)}
 Plotter("", "Graphique de la loi de Malus", x_data, y_data, x_axis_title="Angle relatif (rad)",
-        y_axis_title="Intensité relative", name_file="hallo")
+        y_axis_title="Intensité relative", name_file="hallo", title_font_size =20, sub_font_size=17, step_xticks=0.2, step_yticks=0.05)
+
+# read Csv file
+# myFile = np.genfromtxt(
+#     r"C:\Users\Alexandre\Desktop\alex library python\plotter\Book1.csv", delimiter=',')
+# x_data = myFile[:, 0]
+# y_data = {"valeur": myFile[:, 1]}
+# Plotter(output_path="", title="", x_data=x_data, y_data=y_data, name_file="",
+#         x_axis_title="", y_axis_title="")
